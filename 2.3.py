8b62c6f35e9aa5cbb5f4759e4f1ab3e1e857b728
irinamarkevych80-eng <irinamarkevych80@gmail.com>
2025-09-20 17:51:44 +0200

    Add 2.3

--- 2.3.py
+++ 2.3.py
@@ -1,16 +1,29 @@
 def tomorrow_data(data):
+    """Функция принимает дату и возвращает завтрашнюю дату.
+    Args:
+        data (dict): Словарь с ключами "day", "month", "year".
+    Returns:
+        dict: Словарь с ключами "day", "month", "year".
+    """
     data['day'] += 1
-    if (data['day'] > 10):
+    if (data['day'] > 31 or data['day'] > 30):
         data['day'] = 1
         data['month'] += 1
         if (data['month'] > 12):
             data['month'] = 1
             data['year'] += 1
     return data
+
 def yesterday_data(data):
+    """Функция принимает дату и возвращает вчерашнюю дату.
+    Args:
+        data (dict): Словарь с ключами "day", "month", "year".
+    Returns:
+        dict: Словарь с ключами "day", "month", "year".
+    """
     data['day'] -= 1
     if (data['day'] < 1):
-        data['day'] = 10
+        data['day'] = 30
         data['month'] -= 1
         if (data['month'] < 1):
             data['month'] = 12
